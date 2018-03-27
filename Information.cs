86a55296f5420b51bcf9da2f58706fa77ec2a0a9
Jaime Hernández <jhernandez.hs@icloud.com>
2018-03-27 00:57:40 -0300

    ok
    ok

--- Information.cs
+++ Information.cs
@@ -3,7 +3,7 @@
 // Data Access Application Block
 // Tinet Componets
 //===============================================================================
-// Author: Magno Cardona Heck m@vs.cl
+// Author: Jaime Hernandez
 // Component: Globals 
 //===============================================================================
 
